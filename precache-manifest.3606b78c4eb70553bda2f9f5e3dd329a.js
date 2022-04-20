10e166c4eee0f1f6e8fddbd6e6a7b0fb9a4bbddb
eric <benzakin@gmail.com>
2022-04-20 18:39:30 +0200

    Updates

---
+++ precache-manifest.3606b78c4eb70553bda2f9f5e3dd329a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569e5c082405d7bce0d03d17bb4b0151",
+    "url": "/TP3/index.html"
+  },
+  {
+    "revision": "482c14ed1cc36c3c7514",
+    "url": "/TP3/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "77b6c3aae954cf73bbeb",
+    "url": "/TP3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "482c14ed1cc36c3c7514",
+    "url": "/TP3/static/js/2.338e93d7.chunk.js"
+  },
+  {
+    "revision": "77b6c3aae954cf73bbeb",
+    "url": "/TP3/static/js/main.9ee56c32.chunk.js"
+  },
+  {
+    "revision": "9eb26b3e3b57ec70aece",
+    "url": "/TP3/static/js/runtime-main.22a8638a.js"
+  }
+]);
